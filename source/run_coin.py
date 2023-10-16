ec148f1d78d425252ad8f5cb797fa0363ee7e9a6
Harry Zhang <zharry@seas.upenn.edu>
2023-10-16 13:39:43 -0700

    Working on coin

--- source/run_coin.py
+++ source/run_coin.py
@@ -5,6 +5,8 @@ from textworld_express import TextWorldExpressEnv
 from run_gpt import run_chatgpt
 import argparse
 from pathlib import Path
+import pickle
+import backoff
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', default='gpt-4', type=str, help='OpenAI model name.')
@@ -16,7 +18,14 @@ args = parser.parse_args()
 env = TextWorldExpressEnv(envStepLimit=100)
 
 # Set the game generator to generate a particular game (cookingworld, twc, or coin)
-env.load(gameName="coin", gameParams="numLocations=3,numDistractorItems=0,includeDoors=1,limitInventorySize=0")
+NUM_LOCATIONS = 5
+env.load(gameName="coin", gameParams=f"numLocations={NUM_LOCATIONS},numDistractorItems=0,includeDoors=1,limitInventorySize=0")
+
+@backoff.on_exception(backoff.expo, (ConnectionResetError))
+def post_pddl(data):
+    return requests.post('http://solver.planning.domains/solve',
+                        verify=False, json=data).json()
+
 
 def llm_direct(past_prompt, obs, valid_actions):
     if not past_prompt:
@@ -47,35 +56,68 @@ def llm_pddl(past_prompt, obs, valid_actions):
         prompt = past_prompt + [
             {"role": "user", "content": obs + "\n\n" + "Your task is to go to a location you have not been yet. Generate a problem file."},
         ]
-    if step_id == 1:
-        print(prompt)
-        raise SystemExit
-    output = run_chatgpt(prompt, model=args.model, temperature=0)
+    #print(prompt)
+    try:
+        cache = pickle.load(open("cache.pkl", "rb"))
+    except FileNotFoundError:
+        pickle.dump({}, open("cache.pkl", "wb"))
+        cache = pickle.load(open("cache.pkl", "rb"))
+    try:
+        output = cache[(NUM_LOCATIONS, episode_id, step_id)]
+    except KeyError:
+        output = run_chatgpt(prompt, model=args.model, temperature=0)
+        cache[(NUM_LOCATIONS, episode_id, step_id)] = output
+        pickle.dump(cache, open("cache.pkl", "wb"))
 
     #print(output)
     def parse(output):
-        return output[output.find('('):]
+        return output[output.find('('):].split("```")[0].strip()
     #raise SystemExit
     df = open("coin_df.pddl", "r").read()
     pf = parse(output)
+    print(pf)
     prompt += [
         {"role": "assistant", "content": pf},
     ]
     data = {'domain': df,
         'problem': pf}
-    resp = requests.post('http://solver.planning.domains/solve',
-                        verify=False, json=data).json()
-    actions = resp['result']['plan'][:-1]
+    resp = post_pddl(data)
+    #print(resp)
+    actions = resp['result']['plan']
+    #print(actions)
+    if isinstance(actions[0], str):
+        actions.remove('(reach-goal)')
+    elif isinstance(actions[0], dict):
+        actions = [x['name'] for x in actions]
     def map_actions(action):
+        # move action
         # (move kitchen corridor west)
-        action = action.replace("(", "").replace(")", "").split(" ")
-        return action[0] + " " + action[-1]
+        if "(move " in action:
+            action = action.replace("(", "").replace(")", "").split(" ")
+            action = action[0] + " " + action[-1]
+        # open door action
+        # open_door l1
+        elif "(open_door " in action:
+            at_location = ""
+            direction = ""
+            for line in pf.split("\n"):
+                if "(at " in line:
+                    at_location = line.split("(at ")[1].split(")")[0]
+                if "(connected" in line:
+                    loc_source = line.split("(connected ")[1].split(" ")[0]
+                    loc_target = line.split("(connected ")[1].split(" ")[1]
+                    if loc_source == at_location:
+                        direction = line.split("(connected ")[1].split(" ")[2].split(")")[0]
+                        break
+            action = "open door to " + direction
+        return action
+    #print(actions)
     actions = list(map(map_actions, actions))
-    print(actions)
+    #print(actions)
     #raise SystemExit
 
-    taken_action = actions[0]
-    return prompt, taken_action
+    #taken_action = actions[0]
+    return prompt, actions
 
 if args.method == "direct":
     llm = llm_direct
@@ -84,15 +126,25 @@ elif args.method == "pddl":
 
 # Then, randomly generate and play 10 games within the defined parameters
 steps_to_success = []
-for episode_id in range(1):
+for episode_id in range(3,4):
     # First step
     obs, infos = env.reset(seed=episode_id, gameFold="train", generateGoldPath=True)
     print("Gold path: " + str(env.getGoldActionSequence()))
     # Display the observations from the environment.
-    brief_obs = obs.split('\n')[0].split(". ")[0] + ". " + obs.split('\n')[1]
+    def summarize_obs(obs):
+        # If obs only has one line, return it
+        if len(obs.split('\n')) == 1:
+            return obs
+        # Only keep where you are and location informtion
+        else:
+            return obs.split('\n')[0].split(". ")[0] + ". " + obs.split('\n')[1]
+    brief_obs = summarize_obs(obs)
     print(brief_obs)
     past_prompt = []
+    action_queue = []
+    obs_queue = []
     for step_id in range(0, 10):
+        print("Step " + str(step_id))
         # If there is a coin, just take it
         if " coin" in obs:
             taken_action = "take coin"
@@ -101,16 +153,23 @@ for episode_id in range(1):
             valid_actions = sorted(infos['validActions'])
             valid_actions.remove('look around')
             valid_actions.remove('inventory')
-            print(valid_actions)
-            #break
-            past_prompt, taken_action = llm(past_prompt, brief_obs, valid_actions)
+            # Check action queue for remaining actions
+            if not action_queue:
+                if obs_queue:
+                    brief_obs = "\n".join(obs_queue)
+                    obs_queue = []
+                past_prompt, actions = llm(past_prompt, brief_obs, valid_actions)
+                action_queue += actions
+            taken_action = action_queue.pop(0)
 
         # Take that action
         obs, reward, done, infos = env.step(taken_action)
+        brief_obs = summarize_obs(obs)
+        obs_queue.append(brief_obs)
 
         # Display action and the game's feedback.
         print(">", taken_action)
-        print(obs)
+        print(brief_obs)
         if done:
             steps_to_success.append(step_id)
             break
